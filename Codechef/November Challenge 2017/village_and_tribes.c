ee8730ea83838442c0a54856546c3682aa36da54
Jhonn Carlo Valderama Rubico <jcrubico@yahoo.com>
2017-11-16 16:51:53 +0800

    Finished village and tribes

--- Codechef/November Challenge 2017/village_and_tribes.c
+++ Codechef/November Challenge 2017/village_and_tribes.c
@@ -6,70 +6,44 @@ int main(int argc, char *argv[]) {
 	int T = 0;
 	scanf("%d", &T);
 	int i = 0, j = 0, k = 0;
-	int foundA = 0, foundB = 0;
 	int counterA = 0, counterB = 0;
-	int dotA = 0, dotB = 0;
-	int sumA = 0, sumB = 0;
+	int tempSumA = 0, tempSumB = 0;
+	int flagA = 0, flagB = 0;
 	for (i = 1; i <= T; i++) {
 		scanf("%s", data);
 		int stringLength = strlen(data);
 		for (j = 0; j < stringLength; j++) {
 			char ch = data[j];
-			
-			if (ch == 'A' && !foundA) {
+			if (ch == 'A') {
 				counterA++;
-				foundA = 1;
-			} else if (ch == '.' && foundA) {
-				dotA++;
-			} else {
-				if (ch == 'A' && foundA) {
-					counterA++;
-					foundA = 0;
-					for (k = j + 1; k < stringLength; k++) {
-						if (data[k] == 'A') {
-							foundA = 1;
-							break;
-						}
-						if (data[k] == 'B') {
-							foundA = 0;
-							break;
-						}
-					}
-				}
-			} 
-			
-			if (ch == 'B' && !foundB) {
-				counterB++;
-				foundB = 1;
-			} else if (ch == '.' && foundB) {
-				dotB++;
-			} else {
-				if (ch == 'B' && foundB) {
-					counterB++;
-					foundB = 0;
-					for (k = j + 1; k < stringLength; k++) {
-						if (data[k] == 'A') {
-							foundB = 0;
-							break;
-						}
-						if (data[k] == 'B') {
-							foundB = 1;
-							break;
-						}
-					}
-				}
+				counterA += tempSumA;
+				flagA = 1;
+				flagB = 0;
+				tempSumA = 0;
+				tempSumB = 0;
 			}
-			printf("%d %d %d %d\n", counterA, dotA, counterB, dotB);
+			if (ch == 'B') {
+				counterB++;
+				counterB += tempSumB;
+				flagA = 0;
+				flagB = 1;
+				tempSumA = 0;
+				tempSumB = 0;
+			}
+			if (ch == '.' && flagA) {
+				tempSumA++;
+			}
+			if (ch == '.' && flagB) {
+				tempSumB++;
+			}
 		}
-		dotA = 0;
-		foundA = 0;
-		sumA = 0;
+		printf("%d %d\n", counterA, counterB);
 		counterA = 0;
-		
-		dotB = 0;
-		foundB = 0;
-		sumB = 0;
 		counterB = 0;
+		tempSumA = 0;
+		tempSumB = 0;
+		flagA = 0;
+		flagB = 0;
 	}
 }
 

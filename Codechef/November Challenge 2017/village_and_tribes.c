6e655b07bcfd08dacb0b20574169614e19d452d5
Jhonn Carlo Valderama Rubico <jcrubico@yahoo.com>
2017-11-16 14:51:04 +0800

    Post-Update after competition village and tribes

--- Codechef/November Challenge 2017/village_and_tribes.c
+++ Codechef/November Challenge 2017/village_and_tribes.c
@@ -1,110 +1,79 @@
 #include <stdio.h>
 #include <string.h>
 
-int stateMachine(int state, char ch) {
-	switch (state) {
-		case 0:
-			switch (ch) {
-				case 'A':
-					return 1;
-				case 'B':
-					return 5;
-				case '.':
-					return 9;
-			}
-			break;
-		case 1:
-			switch (ch) {
-				case '.':
-					return 2;
-			}
-			break;
-		case 2:
-			switch (ch) {
-				case '.':
-					return 2;
-				case 'A':
-					return 3;
-			}
-			break;
-		case 3:
-			switch (ch) {
-				case '.':
-					return 4;
-			}
-			break;
-		case 4:
-			switch (ch) {
-				case '.':
-					return 4;
-				case 'A':
-					return 3;
-				case 'B':
-					return 5;
-					
-			}
-			break;
-		case 5:
-			switch (ch) {
-				case '.':
-					return 6;
-			}
-			break;
-		case 6:
-			switch (ch) {
-				case '.':
-					return 6;
-				case 'A':
-					return 1;
-			}
-			break;
-		case 7:
-			switch (ch) {
-				case '.':
-					return 8;
-			}
-			break;
-		case 8:
-			switch (ch) {
-				case '.':
-					return 8;
-				case 'B':
-					return 7;
-			}
-			break;
-		case 9:
-			break;
-	}
-}
-
-void villagesAndTribes(char* input) {
-	int i = 0;
-	int state = 0;
-	int aCounter = 0, bCounter = 0;
-	int tempACounter = 0, tempBCounter = 0;
-	int aSum = 0, bSum = 0;;
-	for (i = 0; i < strlen(input); i++) {
-		char ch = input[i];
-		state = stateMachine(state, ch);
-		if (state == 1 || state == 2) {
-			aCounter++;
-		}
-		if (state == 3) {
-			aCounter++;
-			aSum += aCounter;
-			aCounter = 0;
-		}
-	}
-	printf("%d\n", aSum);
-}
-
 int main(int argc, char *argv[]) {
-	char input[100000];
-	int N = 0, i = 0;
-	scanf("%d", &N);
-	for (i = 0; i < N; i++) {
-		scanf("%s", input);
-		villagesAndTribes(input);
+	char data[100000];
+	int T = 0;
+	scanf("%d", &T);
+	int i = 0, j = 0, k = 0;
+	int foundA = 0, foundB = 0;
+	int counterA = 0, counterB = 0;
+	int dotA = 0, dotB = 0;
+	int sumA = 0, sumB = 0;
+	int stateA = 0, stateB = 0;
+	for (i = 1; i <= T; i++) {
+		scanf("%s", data);
+		int stringLength = strlen(data);
+		for (j = 0; j < stringLength; j++) {
+			char ch = data[j];
+			
+			if (ch == 'A' && !foundA) {
+				counterA++;
+				foundA = 1;
+			} else if (ch == '.' && foundA) {
+				dotA++;
+			} else {
+				if (ch == 'A' && foundA) {
+					counterA++;
+					foundA = 0;
+					for (k = j + 1; k < stringLength; k++) {
+						if (data[k] == 'A') {
+							foundA = 1;
+							break;
+						}
+						if (data[k] == 'B') {
+							foundA = 0;
+							break;
+						}
+					}
+				}
+			} 
+			
+			if (ch == 'B' && !foundB) {
+				counterB++;
+				foundB = 1;
+			} else if (ch == '.' && foundB) {
+				dotB++;
+			} else {
+				if (ch == 'B' && foundB) {
+					counterB++;
+					foundB = 0;
+					for (k = j + 1; k < stringLength; k++) {
+						if (data[k] == 'A') {
+							foundB = 0;
+							break;
+						}
+						if (data[k] == 'B') {
+							foundB = 1;
+							break;
+						}
+					}
+				}
+			}
+			printf("%d %d %d %d\n", counterA, dotA, counterB, dotB);
+		}
+		
+		dotA = 0;
+		foundA = 0;
+		sumA = 0;
+		counterA = 0;
+		stateA = 0;
+		
+		dotB = 0;
+		foundB = 0;
+		sumB = 0;
+		counterB = 0;
+		stateB = 0;
 	}
 }
 
